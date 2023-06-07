0c9a1e870a4bab714d179516b0882ae5bc5ac3b1
Rao Awais <ra848683@gmail.com>
2023-06-07 23:57:18 +0500

    Restructuring the homepage

--- frontend/src/Views/navbar/index.jsx
+++ frontend/src/Views/navbar/index.jsx
@@ -1,27 +1,8 @@
 import { useState } from "react";
-import {
-  Box,
-  IconButton,
-  InputBase,
-  Typography,
-  Select,
-  MenuItem,
-  FormControl,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
-import {
-  Search,
-  Message,
-  DarkMode,
-  LightMode,
-  Notifications,
-  Help,
-  Menu,
-  Close,
-} from "@mui/icons-material";
+import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl } from "@mui/material";
+import { Search, Message, Notifications, Help, Menu, Close } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
-import { setMode, setLogout } from "../../state";
+import { setLogout } from "../../state";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from "../../Components/FlexBetween";
 
@@ -32,15 +13,7 @@ const Navbar = () => {
   const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
-  const theme = useTheme();
-  const neutralLight = theme.palette.neutral.light;
-  const dark = theme.palette.neutral.dark;
-  const background = theme.palette.background.default;
-  const primaryLight = theme.palette.primary.light;
-  const alt = theme.palette.background.alt;
-
-//   const fullName = `${user.firstName} ${user.lastName}`;
-const fullName = "Awais";
+  const fullName = "Awais";
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
@@ -76,13 +49,6 @@ const fullName = "Awais";
 
       {isNonMobileScreens ? (
         <FlexBetween gap="2rem">
-          <IconButton onClick={() => dispatch(setMode())}>
-            {theme.palette.mode === "dark" ? (
-              <DarkMode sx={{ fontSize: "25px" }} />
-            ) : (
-              <LightMode sx={{ color: dark, fontSize: "25px" }} />
-            )}
-          </IconButton>
           <Message sx={{ fontSize: "25px" }} />
           <Notifications sx={{ fontSize: "25px" }} />
           <Help sx={{ fontSize: "25px" }} />
@@ -145,16 +111,6 @@ const fullName = "Awais";
             alignItems="center"
             gap="3rem"
           >
-            <IconButton
-              onClick={() => dispatch(setMode())}
-              sx={{ fontSize: "25px" }}
-            >
-              {theme.palette.mode === "dark" ? (
-                <DarkMode sx={{ fontSize: "25px" }} />
-              ) : (
-                <LightMode sx={{ color: dark, fontSize: "25px" }} />
-              )}
-            </IconButton>
             <Message sx={{ fontSize: "25px" }} />
             <Notifications sx={{ fontSize: "25px" }} />
             <Help sx={{ fontSize: "25px" }} />
